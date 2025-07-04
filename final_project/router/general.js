95d18a651fb8d340e91dd1f9237b6528c01610cb
Oluwatomi <tomisinatansuyi@gmail.com>
2025-07-04 17:42:36 -0400

    get books by an author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,14 +17,25 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+    const isbn = req.params.isbn;
+    return res.send(books[isbn]);
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author.toLowerCase();
+    const booksByAuthor = [];
+
+    for(let isbn in books) {
+        if(books[isbn].author.toLowerCase() === author) {
+            booksByAuthor.push({isbn, ...books[isbn]})
+        }
+    }
+
+    if (booksByAuthor.length === 0) {
+        return res.status(404).json({message: "No books found by this author"})
+    }
+    return res.send(booksByAuthor);
 });
 
 // Get all books based on title
