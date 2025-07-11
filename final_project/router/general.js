5257551b2877f8dbb5fe9514357bb3de8e33d8d0
Oluwatomi Taiwo <tomisinatansuyi@gmail.com>
2025-07-11 15:58:35 -0400

    implemented promise and async/await for getAllBooks and getBooksByIsbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -30,14 +30,35 @@ public_users.post("/register", (req, res) => {
   
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  return res.send(JSON.stringify(books, null, 4))
+public_users.get('/',async function (req, res) {
+  try {
+    const result = await res.send(JSON.stringify(books, null, 4));
+    return result
+  } catch (error) {
+    console.error(error.message)
+  }
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
-    return res.send(books[isbn]);
+    // return res.send(books[isbn]);
+
+    new Promise((resolve, reject) => {
+        const book = books[isbn];
+
+        if (book) {
+            resolve(book);
+        } else {
+            reject ("Book not found");
+        }
+    })
+    .then ((book) => {
+        res.status(200).send(book)
+    })
+    .catch ((error) => {
+        res.status(404).send(error.message)
+    })
 });
   
 // Get book details based on author
